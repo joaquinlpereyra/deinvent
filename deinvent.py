1063a3ece1e2d11a2ca0b52ef6815333a6e095e2
joaquinlperyra <joaquinlpereyra@gmail.com>
2020-06-15 15:38:21 -0300

    foo

---
+++ deinvent.py
@@ -0,0 +1,30 @@
+import math
+def eoq(D: float, K: float, h: float) -> int:
+    """
+    The EOQ is the order cuantity that minimizes
+    the total holding costs and ordering costs.
+
+    D: annual demand
+    K: cost of emmiting an order per unit
+    h: cost of storing an unit
+    """
+    return math.ceil(((2*D*K)/h)**(1/2))
+
+def cycle_time(q: float, D: float) -> float:
+    """
+    The cycle team is the order quantity
+    divided by the demand.
+    """
+    return q / D
+
+def total_cost(D: float, K: float, eoq: float) -> float:
+    """
+    The total cost is the cost of ordering plus mantaining
+    """
+    return ordering_cost(D, K, eoq) + mantain_cost(D, K)
+
+def ordering_cost(D: float, K: float, eoq: float) -> float:
+    return K * (D / eoq)
+
+def mantain_cost(D: float, K: float):
+    return K * (D/ 2) # because y/2 is the average amount of items
